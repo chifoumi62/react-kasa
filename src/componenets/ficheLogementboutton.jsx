683ee1f4af26e665de2206447763f4a57c885e01
sebastien <sebastien.denavaut@gmail.com>
2025-05-03 10:55:38 +0200

    fiche logement boutton

--- src/componenets/ficheLogementboutton.jsx
+++ src/componenets/ficheLogementboutton.jsx
@@ -1,24 +1,14 @@
 import arrow from '../assets/arrow_open.png'
+import Boutton from './boutton';
 
-function FicheLogementBoutton({ description}) {
+function FicheLogementBoutton({ logement}) {
+  const element={
+    id:logement.id,
+    title:"description",
+    description:logement.description
+  }
   return (
-    <div className="fiche-logement-body__description">
-      <div className="fiche-logement-body_boutton">
-          <h1>description</h1>
-          <button className="apropos_button" onClick={() => {
-            const descriptionElement = document.querySelector(".descript");
-            descriptionElement.style.display = descriptionElement.style.display === "block" ? "none" : "block";
-            const arrow = document.querySelector(".arrow_descript");
-            arrow.style.transform = arrow.style.transform === "rotate(180deg)" ? "rotate(0deg)" : "rotate(180deg)";
-          }}>
-            <img src={arrow} alt="arrow" className="arrow_descript"/>
-          </button>
-      </div>
-    
-      <div className="descript" style={{ display: "none" }}>
-        <p>{description}</p>
-      </div>
-    </div>
+    <Boutton element={element}/>
   )
 }
 export default FicheLogementBoutton;
