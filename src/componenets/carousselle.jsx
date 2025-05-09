e827155ce103cb7535cf23289575c0797875460a
sebastien <sebastien.denavaut@gmail.com>
2025-05-09 11:22:16 +0200

    modification css + carrousselle

--- src/componenets/carousselle.jsx
+++ src/componenets/carousselle.jsx
@@ -6,14 +6,25 @@ import arrowright from '../assets/arrow_right.png'
 
 function Carrousselle({pictures}) {
 
-    const [index, setIndex] = useState(0);
+    let [index, setIndex] = useState(0);
 
-    const next = () => setIndex((index + 1) % pictures.length);
-    const prev = () => setIndex((index - 1 + pictures.length) % pictures.length);
-
+    let next = () => setIndex((index + 1) % pictures.length);
+    let prev = () => setIndex((index - 1 + pictures.length) % pictures.length);
+    
+    if (pictures.length === 1) {
+        return (
+            <div className="carrousselle">
+                <img src={pictures[index]} alt={`Image ${index+1}`} className="carrousselle_img"/>
+            </div>
+        );
+    }
+    else{
   return (
       <div className="carrousselle">
-            <img src={pictures[index]} alt={`Image ${index}`} className="carrousselle_img"/>
+            <img src={pictures[index]} alt={`Image ${index+1}`} className="carrousselle_img"/>
+            <div className="carrousselle_index">
+                {index + 1} / {pictures.length}
+            </div>
             <div className="carrousselle_buttons">
                     <button onClick={prev} className="carrousselle_buttons__prev">
                         <img src={arrowright} alt="arrow" className="arrow_prev"/>
@@ -21,9 +32,12 @@ function Carrousselle({pictures}) {
                     <button onClick={next} className="carrousselle_buttons__next">
                         <img src={arrowleft} alt="arrow" className="arrow_next"/>
                     </button>
+
             </div>
+            
       </div>
   );
+}
 }
 
 export default Carrousselle;
